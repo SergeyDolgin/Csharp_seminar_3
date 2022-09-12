c18fdf6bcd3c4b21887805da347b398fab30f12d
Sergey <seregad07@mail.ru>
2022-09-13 01:49:44 +0700

    task 23 about QUBE is done

---
+++ task_23/Program.cs
@@ -0,0 +1,25 @@
+﻿string qubeTable(int num)
+{
+    string result = $"{num} -> 1";
+
+    for (int i = 2; i <= num; i++)
+    {
+        result = result + $", {Math.Pow(i, 3)}";
+
+    }
+
+    return result;
+}
+
+try
+{
+    System.Console.Write("Введите число = ");
+    int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    System.Console.WriteLine(qubeTable(num));
+
+
+}
+catch
+{
+    System.Console.WriteLine("Вводите целочисленное значение");
+}
