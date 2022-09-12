44e9c82a4804e7ff40fcbd4bb4aa91205707ed8e
Sergey <seregad07@mail.ru>
2022-09-13 01:25:46 +0700

    task 19 about palindrom is done

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -4,39 +4,35 @@ try
 {
     Console.Write("Enter five-digits number ");
     int n = Convert.ToInt32(Console.ReadLine());
+    int a, b, c, d, e;
     if (n-10000 < 0 || n-100000 > 0)
     {
         Console.WriteLine("You have entered not a five-digits number");
     }
     if (n-10000 > 0 && n-100000 < 0)
     {
-        a = n % 10000; 
-        first digit is "+(n  / 10000)):
-        b = n % 1000;
-        c = n % 100;
-        d = n % 10;
-C
-Console.WriteLine("The first digit is "+(n  / 10000));
-
+        a = n / 10000; 
+        b = (n / 1000) % 10;
+        d = (n / 10) % 10;
+        e = n  % 10;
+        if (a == e)
+        {
+            if (b==d)
+            {
+                Console.WriteLine("This NUMBER is a palidrom ");
+            }
+            else
+            {
+                Console.WriteLine("This NUMBER is not a palidrom ");
+            }
+        }
+        else 
+        {
+            Console.WriteLine("This NUMBER is not a palidrom ");
+        }
     }
 }
-//     if (n-1000 > 0 && n-10000 < 0)
-//     {
-//         Console.WriteLine("three digit is "+(n  / 10) % 10);
-//     }
-//     if (n-10000 > 0 && n-100000 < 0)
-//     {
-//         Console.WriteLine("three digit is "+(n  / 100) % 10);
-//     }
-//     if (n-100000 > 0 && n-1000000 < 0)
-//     {
-//         Console.WriteLine("three digit is "+(n  / 1000) % 10);
-//     }
-//     if (n < 100 )
-//     {
-//         Console.WriteLine("Threre is no third digit");
-//     }
-// }
+
 catch
 {
     Console.WriteLine("You have entered not a number");
